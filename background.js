5274000835cb8f109a671d209c9ed19489a365b7
ryhaberecht <ryh@ryhaberecht.de>
2018-01-22 17:33:57 +0100

    Only act upon certain error codes
    Prevents reloads due to interrupted asynchronous javascript events etc.
    But, this relies on knowing all the important error codes, which may change any time without warning!

--- background.js
+++ background.js
@@ -2,34 +2,69 @@ if (browser.webRequest.onErrorOccurred.hasListener(tryAgain) == false ) {
         
     browser.webRequest.onErrorOccurred.addListener(
         tryAgain,
-        {urls: ["<all_urls>"]}
+        {urls: ["<all_urls>"],
+        types: ["main_frame"]}
     );
 }
 
 var timeoutIds = [];
+var errorsToReactUpon = {};
+errorsToReactUpon["NS_ERROR_MALFORMED_URI"] = true;
+errorsToReactUpon["NS_ERROR_CONNECTION_REFUSED"] = true;
+errorsToReactUpon["NS_ERROR_NET_TIMEOUT"] = true;
+errorsToReactUpon["NS_ERROR_OFFLINE"] = true;
+errorsToReactUpon["NS_ERROR_PORT_ACCESS_NOT_ALLOWED"] = true;
+errorsToReactUpon["NS_ERROR_NET_RESET"] = true;
+errorsToReactUpon["NS_ERROR_INVALID_CONTENT_ENCODING"] = true;
+errorsToReactUpon["NS_ERROR_UNKNOWN_HOST"] = true;
+errorsToReactUpon["NS_ERROR_REDIRECT_LOOP"] = true;
+errorsToReactUpon["NS_ERROR_ENTITY_CHANGED"] = true;
+errorsToReactUpon["NS_ERROR_SOCKET_CREATE_FAILED"] = true;
+errorsToReactUpon["NS_ERROR_NET_INTERRUPT"] = true;
+errorsToReactUpon["NS_ERROR_PROXY_CONNECTION_REFUSED"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RESOLVING"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RESOLVED"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_CONNECTING_TO"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_CONNECTED_TO"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_SENDING_TO"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_WAITING_FOR"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RECEIVING_FROM"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_REQUEST_HEADER"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_REQUEST_BODY_SENT"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RESPONSE_START"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RESPONSE_HEADER"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_RESPONSE_COMPLETE"] = true;
+errorsToReactUpon["NS_ERROR_NET_ON_TRANSACTION_CLOSE"] = true;
 
 function tryAgain(requestDetails)
 {    
-    if (requestDetails !== null && requestDetails !== undefined && requestDetails.type === "main_frame") {
+    if (requestDetails !== null && requestDetails !== undefined) {
         
-        if (timeoutIds != null && timeoutIds != undefined) {
+        if (errorsToReactUpon[requestDetails.error] === true) {
             
-            var timeoutId = timeoutIds[requestDetails.tabId];
-            if (timeoutId != null && timeoutId != undefined) {
-                clearTimeout(timeoutId);
+            if (timeoutIds != null && timeoutIds != undefined) {
+            
+                var timeoutId = timeoutIds[requestDetails.tabId];
+                if (timeoutId != null && timeoutId != undefined) {
+                    clearTimeout(timeoutId);
+                }
             }
+
+            console.log(`[MMTA] failed: ${requestDetails.url} -> ${requestDetails.error}`);
+
+            var gettingItem = browser.storage.local.get('timeout');
+            gettingItem.then((res) => {
+
+                var timeout = res.timeout || 60;
+                console.log("[MMTA] timeout = " + timeout);
+
+                timeoutIds[requestDetails.tabId] = setTimeout(reload, timeout * 1000, requestDetails.tabId);
+            }, onError);
         }
-        
-        console.log(`[MMTA] failed: ${requestDetails.url}`);
-        
-        var gettingItem = browser.storage.local.get('timeout');
-        gettingItem.then((res) => {
+        else {
             
-            var timeout = res.timeout || 60;
-            console.log("[MMTA] timeout = " + timeout);
-            
-            timeoutIds[requestDetails.tabId] = setTimeout(reload, timeout * 1000, requestDetails.tabId);
-        }, onError);
+            console.log(`[MMTA] not reacting upon error code ${requestDetails.error} for URL ${requestDetails.url}`);
+        }
     }
 }
 
